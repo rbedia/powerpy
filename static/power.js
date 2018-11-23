7721734a124d17e008cdde8455fd6b75933332c0
Rafael Bedia <dcat@trillinux.org>
2018-11-23 15:00:48 +0000

    Go back to using the original URL for files.

--- static/power.js
+++ static/power.js
@@ -10,8 +10,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// var powerTileUrl = 'http://power.doxu.org/osm_tiles/{z}/{x}/{y}.png';
-var powerTileUrl = 'http://aqua.trillinux.org:8112/osm_tiles/{z}/{x}/{y}.png';
+var powerTileUrl = 'http://power.doxu.org/osm_tiles/{z}/{x}/{y}.png';
 var powerMap = L.tileLayer(powerTileUrl, {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
